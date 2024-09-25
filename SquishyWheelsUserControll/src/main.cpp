12f71da393c952e0cbdfac2625ea75d304c32721
Eli-Sur <135772894+Eli-Sur@users.noreply.github.com>
2024-09-25 12:38:46 -0500

    Implement drivercode (#2)
    * Define brain and motors
    
    * add user control over robot

--- SquishyWheelsUserControll/src/main.cpp
+++ SquishyWheelsUserControll/src/main.cpp
@@ -16,6 +16,79 @@ competition Competition;
 
 // define your global instances of motors and other devices here
 
+#pragma region VEXcode Generated Robot Configuration
+// Make sure all required headers are included.
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+
+// Brain should be defined by default
+brain Brain;
+
+
+// START V5 MACROS
+#define waitUntil(condition)                                                   \
+  do {                                                                         \
+    wait(5, msec);                                                             \
+  } while (!(condition))
+
+#define repeat(iterations)                                                     \
+  for (int iterator = 0; iterator < iterations; iterator++)
+// END V5 MACROS
+
+
+// Robot configuration code.
+motor leftTopMotor = motor(PORT1, ratio18_1, false);
+
+motor rightTopMotor = motor(PORT10, ratio18_1, true);
+
+motor leftBackMotor = motor(PORT11, ratio18_1, false);
+
+motor rightBackMotor = motor(PORT20, ratio18_1, true);
+
+motor intakeRamp = motor(PORT7, ratio18_1, false);
+
+controller Controller1 = controller(primary);
+
+
+// generating and setting random seed
+void initializeRandomSeed(){
+  int systemTime = Brain.Timer.systemHighResolution();
+  double batteryCurrent = Brain.Battery.current();
+  double batteryVoltage = Brain.Battery.voltage(voltageUnits::mV);
+
+  // Combine these values into a single integer
+  int seed = int(batteryVoltage + batteryCurrent * 100) + systemTime;
+
+  // Set the seed
+  srand(seed);
+}
+
+
+
+void vexcodeInit() {
+
+  //Initializing random seed.
+  initializeRandomSeed(); 
+}
+
+
+// Helper to make playing sounds from the V5 in VEXcode easier and
+// keeps the code cleaner by making it clear what is happening.
+void playVexcodeSound(const char *soundName) {
+  printf("VEXPlaySound:%s\n", soundName);
+  wait(5, msec);
+}
+
+
+
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+#pragma endregion VEXcode Generated Robot Configuration
+
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
 /*                                                                           */
@@ -27,7 +100,11 @@ competition Competition;
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
-
+  leftTopMotor.setVelocity(100.0, percent);
+  leftBackMotor.setVelocity(100.0, percent);
+  rightBackMotor.setVelocity(100.0, percent);
+  rightTopMotor.setVelocity(100.0, percent);
+  intakeRamp.setVelocity(100.0, percent);
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
 }
@@ -64,7 +141,11 @@ void usercontrol(void) {
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
-
+    leftTopMotor.spin(forward, (Controller1.Axis3.position() + Controller1.Axis4.position()) / 90.0 * 12, volt);
+    leftBackMotor.spin(forward, (Controller1.Axis3.position() + Controller1.Axis4.position()) / 90.0 * 12, volt);
+    rightTopMotor.spin(forward, (Controller1.Axis3.position() - Controller1.Axis4.position()) / 90.0 * 12, volt);
+    rightBackMotor.spin(forward, (Controller1.Axis3.position() - Controller1.Axis4.position()) / 90.0 * 12, volt);
+    intakeRamp.spin(forward, Controller1.Axis2.position() / 90.0 * 12, volt);
     // ........................................................................
     // Insert user code here. This is where you use the joystick values to
     // update your motors, etc.
